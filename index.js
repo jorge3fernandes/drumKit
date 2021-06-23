90948c566ff454bcc08dfa6fd040722480bccbd1
jorge3fernandes <jorge3fernandes@gmail.com>
2021-06-23 06:46:34 -0400

    My first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var buttons = document.querySelectorAll(".drum")
+
+for (var index = 0; index < buttons.length; index++)
+    buttons[index].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
+        
+    })
+
+document.addEventListener("keydown", function(event){
+
+    key = event.key
+    playSound(key)
+    buttonAnimation(key)
+
+})
+
+function playSound(buttonInnerHTML){
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML);
+    }
+
+}
+
+function buttonAnimation(currentKey){
+    var key = document.querySelector("."+currentKey);
+    key.classList.add("pressed");
+
+    setTimeout( function(){
+        key.classList.remove("pressed");
+    }, 100);
+};
